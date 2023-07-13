00faa03f98e1569fdc9497ef4d2cd0e71044e94a
bitcamp\AIA <agatha2003@naver.com>
2023-07-13 18:42:22 +0900

    plugins { id 'java' id 'org.springframework.boot' version '3.1.0' id 'io.spring.dependency-management' version '1.1.0' }
    group = 'com.spring'
    version = '0.0.1-SNAPSHOT'
    sourceCompatibility = '17'
    
    configurations {
    compileOnly {
    extendsFrom annotationProcessor
    }
    }
    
    repositories {
    mavenCentral()
    }
    
    dependencies {
    implementation 'org.springframework.boot:spring-boot-starter-web'
    implementation 'org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.2'
    testImplementation 'org.projectlombok:lombok:1.18.26'
    compileOnly 'org.projectlombok:lombok'
    annotationProcessor 'org.projectlombok:lombok'
    testImplementation 'org.springframework.boot:spring-boot-starter-test'
    implementation 'mysql:mysql-connector-java:8.0.28'
    implementation 'org.apache.tomcat.embed:tomcat-embed-jasper'
    implementation group:
    
    exit
    !q

--- src/main/java/com/spring/blog/entity/Reply.java
+++ src/main/java/com/spring/blog/entity/Reply.java
@@ -1,17 +1,41 @@
 package com.spring.blog.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 // Entity는 불변성을 지키기 위해 Setter를 제공하지 않습니다.
 // 한 번 생성된 데이터가 변경될 가능성을 없앱니다.
-@Getter @ToString @AllArgsConstructor @Builder @NoArgsConstructor
+@Getter @ToString @AllArgsConstructor
+@Builder @NoArgsConstructor @Setter @Entity
 public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long replyId;
+
+    @Column(nullable = false)
     private long blogId;
+
+    @Column(nullable = false)
     private String replyWriter;
+
+    @Column(nullable = false)
     private String replyContent;
+
     private LocalDateTime publishedAt;
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void setDefaultValue(){
+        this.publishedAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void setUpdatedAt(){
+        this.updatedAt = LocalDateTime.now();
+    }
+
 }
